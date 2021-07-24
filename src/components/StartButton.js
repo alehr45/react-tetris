7f2fea50a9f2ae85cd959f022bd5d1e4f7623585
Your NameAlex <alehr45@gmail.com>
2021-07-24 07:43:06 -0500

    stage and display and start button

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { StyledStartButton } from './styles/StyledStartButton';
 
 const StartButton = ({ callback }) => (
-    <div>Start Game</div>
+    <StyledStartButton onCLick={callback}>Start Game</StyledStartButton>
 );
 
 export default StartButton;
