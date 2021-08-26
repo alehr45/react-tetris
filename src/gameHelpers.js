6226198c19433182ef8401b839a918488978ca2e
Your NameAlex <alehr45@gmail.com>
2021-08-26 11:05:48 -0500

    added collision detection for tetrominos

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -1,8 +1,29 @@
 export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
-export const createStage = () => 
-Array.from(Array(STAGE_HEIGHT), () => 
- new Array(STAGE_WIDTH).fill([0, 'clear'])
-)
+export const createStage = () =>
+  Array.from(Array(STAGE_HEIGHT), () =>
+    new Array(STAGE_WIDTH).fill([0, "clear"])
+  );
 
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+  for (let y = 0; y < player.tetromino.length; y += 1) {
+    for (let x = 0; x < player.tetromino[0].length; x += 1) {
+      //Check that were on an actual Tetromino cell
+      if (player.tetromino[y][x] !== 0) {
+        if (
+            //Check that movement is inside the game areas height (y)
+          //We should not go through bottom of play area
+          !stage[y + player.pos.y + moveY] ||
+          //Check that movement is inside the game areas width (x)
+          !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+          //Check that the cell that were moving to isn't set to clear
+          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
+            "clear"
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+};
